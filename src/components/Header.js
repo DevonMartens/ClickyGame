c7ac9282207227532f8edcb775eab041294bde96
DevonMartens <dmmartens91@gmail.com>
2020-01-14 17:14:08 -0500

    outline complete

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="container">
+      <h1 className="page-title">Simspons Fan Game</h1>
+      <h3 className="page-subTitle">Only click each character once! Get them all and Win!</h3>
+    <br/>
+    </div>
+  );
+}
+
+export default Header;
