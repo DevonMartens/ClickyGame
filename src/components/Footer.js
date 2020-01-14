4c9dd6721033a6c847a12057d2650c9b827b06be
DevonMartens <dmmartens91@gmail.com>
2020-01-14 17:10:52 -0500

    footer added add image of simpsons logo to images folder

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+
+    <footer>
+        <img className="SimpsonsLogo" src="" alt="logo" /> 
+        <span id="footer-right">A Simpsons Memory Game DOH!</span> 
+    </footer>
+  );
+}
+
+export default Footer;
