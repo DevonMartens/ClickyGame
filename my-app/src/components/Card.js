7e891091c82a851aa68ce0da3cedfe47fa02a234
DevonMartens <dmmartens91@gmail.com>
2020-01-16 09:31:33 -0500

    changing css

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -5,14 +5,9 @@ function Card(props) {
 
   return (
       <div className="card visible" onClick={() => props.clicked(props.id)}>
-        {/* <div className="card-face"> */}
-          {/* <img src="" alt="twinkle" className="twinkle" /> */}
+       
           <div className="overlay"></div>
-          {/* <img src="assets/imgs/movie-reel.png" alt="" className="reel" />
-          <img className="movie-reel movie-reel-top-left" src="" alt="movie" />
-          <img className="movie-reel movie-reel-top-right" src="" alt="movie" />
-          <img className="movie-reel movie-reel-bottom-left" src="" alt="movie" />
-          <img className="movie-reel movie-reel-bottom-right" src="" alt="movie" /> */}
+          
           <img className="character" src={"assets/images/" + props.characterImage} alt="character" />
           <div id="character-name" key={props.id}>{props.characterName}</div>
         {/* </div> */}
