d22b9ca41bc684a54d9d1bd5d4c1ec17f4b34dc3
DevonMartens <dmmartens91@gmail.com>
2020-01-19 11:41:51 -0500

    fixing 404 github error

---
+++ my-app/src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css"
+
+function Card(props) {
+
+  return (
+      <div className="card visible" onClick={() => props.clicked(props.id)}>
+       
+          <div className="overlay"></div>
+          
+          <img className="character" src={"/assets/images/" + props.characterImage} alt="character" />
+          <div id="character-name" key={props.id}>{props.characterName}</div>
+    
+      </div>
+  );
+}
+
+export default Card;
