c116570f4d29fee74f52a1444b7e7dac598ca4d7
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-02-02 08:57:12 -0500

    production build

---
+++ build/precache-manifest.08c5767e1a1996020f962a581aa64a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d829519e249f103bfb6a47adb07d99",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "eb8524af2560d90f1c0d",
+    "url": "/ClickyGame/static/css/main.bb125610.chunk.css"
+  },
+  {
+    "revision": "bca1daf7d1ba410c5205",
+    "url": "/ClickyGame/static/js/2.230117c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.230117c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb8524af2560d90f1c0d",
+    "url": "/ClickyGame/static/js/main.8fc580e0.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  }
+]);
